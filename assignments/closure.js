89afd9f64e08e782f310b009ae989af2bd27fe78
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-01-22 18:34:20 -0500

    woked on closures

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,15 +1,22 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+const language = "english";
+  demand = ()=> {
+    let answer="What?"
+    return (language + 'do you speak it? :'+ answer)
+  }
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
+const counter = () => { 
+  let cc = 0; 
+ 
+    return cc += 1;
+  
   // Return a function that when invoked increments and returns a counter variable.
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
-
+const freedcounter = counter();
+console.log(freedcounter);
+console.log(freedcounter);
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
